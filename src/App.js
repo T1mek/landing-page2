8a50eaa9d3c05dd7836f7d76ea4ab39cdccc6a27
Михаил <misha1234567890m@gmail.com>
2022-07-11 15:48:38 +0300

    Добавлени стилей

---
+++ src/App.js
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div className="app">
+    
+      <div className="main">
+        <div className="logo">
+          <img src="/img/logo.png" alt="Logo" />
+          
+        </div>
+        <p>EMBRACING EVERY STEP</p>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Shop</li>
+          <li>Contact</li>
+        </ul>
+        <div className="search">
+          <img src="/img/search.png" alt="Search" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
