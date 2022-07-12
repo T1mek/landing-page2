fc7bec2398c732b886eacd71baf46eb7f9baadf2
Михаил <misha1234567890m@gmail.com>
2022-07-12 18:00:31 +0300

    Закончил верстку landing-page

--- src/App.js
+++ src/App.js
@@ -18,22 +18,28 @@ function App() {
             <img src="/img/search.png" alt="Search" />
           </div>
         </div>
-        <div className="title">
-          <h1>
-            FOR EVERY STEP, <br></br>{" "}
-            <b>
-              WE HAVE <br></br>SOMETHING
-            </b>
-          </h1>
-        </div>
-        <div className="picture">
-          <div className="click">
-            <button>
-              <b>SHOP</b>
-            </button>
-            <img src="/img/bold.png" alt="bold" />
+        <div className="footer">
+          <div className="title">
+            <div className="text">
+              <h1>
+                FOR EVERY STEP, <br></br>{" "}
+                <b>
+                  WE HAVE <br></br>SOMETHING
+                </b>
+              </h1>
+
+              <div className="click">
+                <button>
+                  <b>SHOP</b>
+                </button>
+                <img src="/img/bold.png" alt="bold" />
+              </div>
+            </div>
+
+            <div className="p1">
+              <img src="/img/main.png" alt="" />
+            </div>
           </div>
-          <img className="p1" src="/img/main.png" alt="" />
         </div>
       </div>
     </div>
