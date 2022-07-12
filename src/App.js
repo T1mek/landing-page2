e5535ac1357168d15bb9d797e477b158b49afaa0
Михаил <misha1234567890m@gmail.com>
2022-07-12 15:33:15 +0300

    Добавление стилей

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 function App() {
   return (
     <div className="app">
-    
       <div className="main">
-        <div className="logo">
-          <img src="/img/logo.png" alt="Logo" />
-          
+        <div className="header">
+          <div className="logo">
+            <img src="/img/logo.png" alt="Logo" />
+            <p>EMBRACING EVERY STEP</p>
+          </div>
+
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Shop</li>
+            <li>Contact</li>
+          </ul>
+          <div className="search">
+            <img src="/img/search.png" alt="Search" />
+          </div>
         </div>
-        <p>EMBRACING EVERY STEP</p>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Shop</li>
-          <li>Contact</li>
-        </ul>
-        <div className="search">
-          <img src="/img/search.png" alt="Search" />
+        <div className="title">
+          <h1>
+            FOR EVERY STEP, <br></br>{" "}
+            <b>
+              WE HAVE <br></br>SOMETHING
+            </b>
+          </h1>
+        </div>
+        <div className="picture">
+          <div className="click">
+            <button>
+              <b>SHOP</b>
+            </button>
+            <img src="/img/bold.png" alt="bold" />
+          </div>
+          <img className="p1" src="/img/main.png" alt="" />
         </div>
       </div>
     </div>
